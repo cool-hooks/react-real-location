e150e09a5c702bdb8dfa206ec36e0ecceff16bbe
Jakub Biesiada <kbba@op.pl>
2021-04-03 22:48:59 +0200

    chore: replace useEffect with useMemo (#10)

--- src/useRealLocation.ts
+++ src/useRealLocation.ts
@@ -1,16 +1,8 @@
-import { useState, useEffect } from 'react';
+import { useMemo } from 'react';
 import { useLocation } from 'react-router';
 
 export const useRealLocation = () => {
   const location = useLocation();
 
-  const [realLocation, setRealLocation] = useState(location);
-
-  useEffect(() => {
-    setRealLocation(location);
-
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [window?.location]);
-
-  return realLocation;
+  return useMemo(() => location, [window?.location]);
 };
