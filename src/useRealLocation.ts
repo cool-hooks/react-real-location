70eeb6b695cb9b930ed49778f1643707b77e7229
Jakub Biesiada <biesiadajakub@icloud.com>
2021-06-10 21:02:07 +0200

    chore: update

--- src/useRealLocation.ts
+++ src/useRealLocation.ts
@@ -4,5 +4,6 @@ import { useLocation } from 'react-router';
 export const useRealLocation = () => {
   const location = useLocation();
 
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   return useMemo(() => location, [window?.location]);
 };
